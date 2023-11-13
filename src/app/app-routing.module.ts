7abd3e3b34e6813d3bf3d543e1dbdfa772189f98
ipiwixx <antoine13141@gmail.com>
2023-11-13 12:55:27 +0100

    new modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LdapListComponent} from "./ldap-list/ldap-list.component";
+import {PagNotFoundComponent} from "./pag-not-found/pag-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users/list', component: LdapListComponent },
+  { path: '**', component: PagNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LdapListComponent } from './ldap-list/ldap-list.component';
 import { PagNotFoundComponent } from './pag-not-found/pag-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     LdapListComponent,
-    PagNotFoundComponent
+    PagNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
